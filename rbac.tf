309e183a79ab724946f519bff50f83bc9c3e65ba
igkat <104429325+igkat@users.noreply.github.com>
2022-08-26 15:25:25 +0200

    Update rbac.tf

--- rbac.tf
+++ rbac.tf
@@ -1,9 +1,5 @@
 data "azuread_client_config" "current" {}
 
-data "azuread_user" "aad_user" {
-  for_each            = toset(var.avd_users)
-  user_principal_name = format("%s", each.key)
-}
 
 data "azurerm_role_definition" "role" { # access an existing built-in role
   name = "Desktop Virtualization User"
